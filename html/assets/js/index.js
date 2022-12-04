3f3ccafc657e300031ece781351b8b6288e42bb8
ZariTaghavi <taghavi.zari@gmail.com>
2022-12-04 19:44:15 +0330

    build: add time function to show current time

--- html/assets/js/index.js
+++ html/assets/js/index.js
@@ -1,1 +1,28 @@
+function formatWeekday(time) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `${days[time.getDay()]}`;
+}
 
+function formatClock(time) {
+  let hour = time.getHours();
+  let minute = time.getMinutes();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${hour}:${minute}`;
+}
+
+let now = new Date();
+let todayTime = document.querySelector("#today-time");
+todayTime.innerHTML = `${formatWeekday(now)}, ${formatClock(now)}`;
