ca141ea9ce1ce9d39ee23231712657ec96dc6f23
ZariTaghavi <taghavi.zari@gmail.com>
2022-12-04 19:54:58 +0330

    build: add temperature degree convert functions

--- html/assets/js/index.js
+++ html/assets/js/index.js
@@ -39,3 +39,35 @@ function showLocation(event) {
 
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", showLocation);
+
+let farenheitflag = true;
+let celciusflag = true;
+function convertToFarenheit(event) {
+  event.preventDefault();
+  if (farenheitflag) {
+    let temperature = document.querySelector("h2");
+    let temperatureValue = temperature.innerText;
+    temperatureValue = Number(temperatureValue);
+    let farenheit = Math.round(temperatureValue * 1.8 + 32);
+    temperature.innerHTML = farenheit;
+  }
+  farenheitflag = false;
+  celciusflag = true;
+}
+let farenheitLink = document.querySelector("#farenheit");
+farenheitLink.addEventListener("click", convertToFarenheit);
+
+function convertToCelcius(event) {
+  if (celciusflag) {
+    event.preventDefault();
+    let temperature = document.querySelector("h2");
+    let temperatureValue = temperature.innerText;
+    temperatureValue = Number(temperatureValue);
+    let celcius = Math.round(((temperatureValue - 32) * 5) / 9);
+    temperature.innerHTML = celcius;
+  }
+  celciusflag = false;
+  farenheitflag = true;
+}
+let celciusLink = document.querySelector("#celcius");
+celciusLink.addEventListener("click", convertToCelcius);
