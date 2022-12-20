5a39d74e896e38076b5727c956c3f5ecaa8eee16
ZariTaghavi <taghavi.zari@gmail.com>
2022-12-20 14:50:31 +0330

    build: add a fucnction to show weather icon

--- html/assets/js/index.js
+++ html/assets/js/index.js
@@ -39,6 +39,7 @@ let farenheitLink = document.querySelector("#farenheit");
 let currentLocationButton = document.querySelector("#current-location");
 let updateLink = document.querySelector("#update");
 
+let iconElement = document.querySelector("#weather-icon");
 let cityElement = document.querySelector("#city");
 let countryElement = document.querySelector("#country");
 let temperatureElement = document.querySelector("h2");
@@ -53,12 +54,56 @@ function updateTime(time) {
   todayTime.innerHTML = `${formatWeekday(time)}, ${formatClock(time)}`;
 }
 
+function showWeathericon(iconId, iconTime) {
+  iconElement.setAttribute("class", "fa-solid");
+  if (200 <= iconId && iconId < 300) {
+    iconElement.classList.add("fa-cloud-bolt");
+  } else if (300 <= iconId && iconId < 400) {
+    iconElement.classList.add("fa-cloud-rain");
+  } else if (502 <= iconId && iconId < 600 && iconId == !511) {
+    iconElement.classList.add("fa-cloud-showers-heavy");
+  } else if (500 <= iconId && iconId <= 501 && iconTime == "10d") {
+    iconElement.classList.add("fa-cloud-sun-rain");
+  } else if (500 <= iconId && iconId <= 501 && iconTime == "10n") {
+    iconElement.classList.add("fa-cloud-moon-rain");
+  } else if ((600 <= iconId && iconId < 700) || iconId == 511) {
+    iconElement.classList.add("fa-snowflake");
+  } else if ((700 <= iconId && iconId <= 761) || iconId == 771) {
+    iconElement.classList.add("fa-smog");
+  } else if (iconId == 762) {
+    iconElement.classList.add("fa-volcano");
+  } else if (iconId == 781) {
+    iconElement.classList.add("fa-tornado");
+  } else if (iconId == 800 && iconTime == "01d") {
+    iconElement.classList.add("fa-sun");
+  } else if (iconId == 800 && iconTime == "01n") {
+    iconElement.classList.add("fa-moon");
+  } else if (iconId == 801 && iconTime == "02d") {
+    iconElement.classList.add("fa-cloud-sun");
+  } else if (iconId == 801 && iconTime == "02n") {
+    iconElement.classList.add("fa-cloud-moon");
+  } else if (802 <= iconId && iconId <= 804) {
+    iconElement.classList.add("fa-cloud");
+    if (iconId == 802) {
+      iconElement.classList.add("scattered-clouds");
+    } else if (iconId == 803) {
+      iconElement.classList.add("broken-clouds");
+    } else if (iconId == 804) {
+      iconElement.classList.add("overcast-clouds");
+    }
+  }
+}
+
 function showWeatherData(response) {
   let timeValue = new Date();
+  let iconIdValue = response.data.weather[0].id;
+  let iconTimeValue = response.data.weather[0].icon;
   let temperatureValue = Math.round(response.data.main.temp);
   let descriptionValue = response.data.weather[0].description;
   let humidityValue = Math.round(response.data.main.humidity);
   let windSpeedValue = Math.round(response.data.wind.speed);
+
+  showWeathericon(iconIdValue, iconTimeValue);
   updateTime(timeValue);
   temperatureElement.innerHTML = `${temperatureValue}`;
   descriptionElement.innerHTML = `${descriptionValue}`;
